6797e9316ea5f3ac40e925639baa2a3c3b71f309
Ксения Гуревич <kurvinkl@gmail.com>
2022-09-18 17:00:38 +0300

    path to media4

--- backend/api/models.py
+++ backend/api/models.py
@@ -61,7 +61,7 @@ class Recipe(models.Model):
         verbose_name='Описание рецепта',
     )
     image = models.ImageField(
-        upload_to='/app/media/',
+        upload_to='.',
         verbose_name='Фото рецепта'
     )
     tags = models.ManyToManyField(
