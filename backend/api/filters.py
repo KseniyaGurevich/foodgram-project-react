cc1532a618a5a5b8a65d8782e51ef6cbe638a9cc
Ксения Гуревич <kurvinkl@gmail.com>
2022-09-18 16:17:35 +0300

    path to media

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,11 +1,20 @@
-import django_filters
+#import django_filters
+from django_filters import filters
 
 from .models import Recipe
 
 
-class Filter(django_filters.FilterSet):
-    author = django_filters.NumberFilter(field_name='author__id')
-    tags = django_filters.CharFilter(field_name='tags__slug')
+class Filter(filters.FilterSet):
+    author = filters.NumberFilter(field_name='author__id')
+    tags = filters.CharFilter(field_name='tags__slug')
+    is_favorited = filters.ChoiceFilter(
+        choices=enumerate([0, 1]),
+        method='filter_is_favorited'
+    )
+    is_in_shopping_cart = filters.ChoiceFilter(
+        choices=enumerate([0, 1]),
+        method='filter_is_in_shopping_cart'
+    )
 
     class Meta:
         model = Recipe
